6d4f262b7f5c1dad97e47fe1af03833b54ce9052
Manat Tabtim <klamanat@gmail.com>
2026-03-24 12:32:03 +0700

    feat: update

--- app/src/main.js
+++ app/src/main.js
@@ -8,6 +8,7 @@ import { inject } from '@vercel/analytics'
 inject()
 
 // ล้าง localStorage cache เก่าที่อาจมี base64 images ขนาดใหญ่
+
 // เพิ่ม version ทุกครั้งที่ cache structure เปลี่ยน
 ;(function purgeLegacyCache() {
   const CACHE_VER = 'dsc_v2'
